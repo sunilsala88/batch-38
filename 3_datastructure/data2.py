5027e23d84ce2aef043cde84fdc19f631ee5eb12
sunilsala88 <sunilsala88@gmail.com>
2026-07-11 19:28:17 +0530

    jul-11-6

---
+++ 3_datastructure/data2.py
@@ -0,0 +1,27 @@
+
+
+l1=[33,44,55,66,55,66]
+print(l1)
+#add
+l1.append(100)
+l1.insert(1,200)
+print(l1)
+
+#index and slice
+print(l1[0])
+print(l1[-2:])
+
+#remove
+l1.remove(66)
+print(l1)
+l1.pop(-1)
+print(l1)
+
+#index
+print(l1.index(55))
+print(l1)
+
+
+#old way of removing element
+del l1[1]
+print(l1)
