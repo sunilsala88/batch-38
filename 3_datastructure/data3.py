cddda2672398c74ed9b508779fd2c8eafcc318fd
sunilsala88 <sunilsala88@gmail.com>
2026-07-12 18:45:14 +0530

    jul-12-4

--- 3_datastructure/data3.py
+++ 3_datastructure/data3.py
@@ -1,7 +1,8 @@
 
 #keys are always unique
 stock_prices={'tsla':1000,'apple':2000,'microsoft':3000,'google':4000}
-stock_number={1:'tsla',2:'apple',3:'microsoft',4:'google',5:True}
+stock_number={1:'tsla',2:'apple',3:'microsoft',4:'google',5:True,4:'kgoogle'}
+print(stock_number)
 
 #access
 v1=stock_prices.get('apple')
@@ -12,13 +13,19 @@ print(v2)
 
 #add
 a1=stock_prices.update({'nifty':5000})
+
+stock_prices['banknifty']=900
 print(stock_prices)
+
+
 
 #update
 a1=stock_prices.update({'google':6000})
+stock_prices['tsla']=2000
 print(stock_prices)
 
 #remove
 r1=stock_prices.pop('tsla')
+del stock_prices['google']
 print(stock_prices)
 
