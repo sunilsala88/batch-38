767edb8cd9d7f0b273af31f596e0c830d6d663d5
sunilsala88 <sunilsala88@gmail.com>
2026-07-25 18:28:28 +0530

    jul-25-1

---
+++ 5_loops/loop1.py
@@ -0,0 +1,29 @@
+
+# #type 1
+# l1=[33,44,55,66,77]
+# for i in l1:
+#     print(i)
+
+# #type 2
+# for i in range(10):
+#     print('hello')
+
+# #type 3
+# for i in range(len(l1)):
+#     print(l1[i])
+
+#typ3 4
+
+#not type 4
+stock_prices={'AAPL': 200, 'GOOG': 300, 'MSFT': 400}
+# for i in stock_prices:
+#     print(i, stock_prices.get(i))
+
+print('-------------------')
+print(list(stock_prices.keys()))
+print(list(stock_prices.values()))
+print(list(stock_prices.items()))
+
+#type 4
+for i,j in stock_prices.items():
+    print(i,j)
