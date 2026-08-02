18efcd65239e35a39f637e85d196287d1e122ae9
sunilsala88 <sunilsala88@gmail.com>
2026-08-02 19:05:57 +0530

    aug-2-88

---
+++ 6_function/function4.py
@@ -0,0 +1,21 @@
+
+
+
+#argument ,parameter
+
+
+def fun1(a,b):#2,3
+    n=a**2 +b
+    return n
+
+
+def fun2(x,y): #2,3
+    z=fun1(x,y) #7
+    return z+y #7+3
+
+def fun3(p,q):
+    r=fun2(p,q) #10
+    u=fun1(p,q) #7
+    return r+u
+
+print(fun3(2,3))
