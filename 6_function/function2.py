7e45bb2a32db6d135b4bb622805e9109e2b60a56
sunilsala88 <sunilsala88@gmail.com>
2026-08-02 18:44:35 +0530

    aug-2-8

--- 6_function/function2.py
+++ 6_function/function2.py
@@ -21,4 +21,20 @@ num=11
 print(is_odd(num))
 print(is_even(num))
 
+
+def reverse(string1:str)->str:
+    rev_str=''
+    for i in string1:
+        rev_str=i+rev_str
+    return rev_str
+
+
+def is_palindrome(string1:str)->bool:
+    rev_str=reverse(string1)
+    if string1==rev_str:
+        return True
+    else:
+        return False
+
+print(is_palindrome('madam'))
     
---
+++ 6_function/function3.py
@@ -0,0 +1,14 @@
+
+a=30
+
+def fun1():
+    global a
+    print(temp)
+    a=20
+    print(a)
+    b=20
+
+
+fun1()
+temp='some value'
+print(a)
