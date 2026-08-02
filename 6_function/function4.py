c98f0b7900cb1a904f2b6d0bd662586217bdbdbf
sunilsala88 <sunilsala88@gmail.com>
2026-08-02 19:12:09 +0530

    aug-2-44

--- 6_function/function4.py
+++ 6_function/function4.py
@@ -19,3 +19,17 @@ def fun3(p,q):
     return r+u
 
 print(fun3(2,3))
+
+#1. Positional Arguments
+def greet_user(name, age):
+    print(f"Hello {name}, you are {age} years old.")
+
+greet_user(25,"Alice")
+
+
+#Default Arguments
+def greet(name='User'):
+    print("Hello", name)
+greet()
+
+#. Keyword Arguments
