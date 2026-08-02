b860d95b97945cf4b4b9a184a83975afb394bb69
sunilsala88 <sunilsala88@gmail.com>
2026-08-02 18:26:00 +0530

    aug-2-1

--- 6_function/function1.py
+++ 6_function/function1.py
@@ -72,13 +72,15 @@ print(fl)
 
 def add(num1:int,num2:int)->float:
     total=num1+num2
+    return total
 
-num1=10
-num2=20
-result=add(num1,num2)
-print(result)
+def main():
+    num1=10
+    num2=20
+    result=add(num1,num2)
+    print(result)
 
 
-
+main()
 
 #add,sub,mul div
---
+++ 6_function/function2.py
@@ -0,0 +1,24 @@
+
+
+
+#palindrome
+#
+
+def is_odd(num:int)->bool:
+    if num%2==0:
+        return False
+    else:
+        return True
+
+
+def is_even(num:int)->bool:
+    if num%2==0:
+        return True
+    else:
+        return False
+
+num=11
+print(is_odd(num))
+print(is_even(num))
+
+    
