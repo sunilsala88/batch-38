97c51cbf5ebdbe570198ab3e9e90cc5512e6b838
sunilsala88 <sunilsala88@gmail.com>
2026-08-15 19:05:18 +0530

    aug-15-6

---
+++ 9_module/file3.py
@@ -0,0 +1,21 @@
+
+
+import time
+print(time.time())
+
+print('line 1')
+time.sleep(1)
+print('line 2')
+
+import os
+
+print(os.getcwd())
+
+
+import sys
+sys.exit()
+
+print('last line')
+
+import numpy as np
+np.array([1,2,3,4,5])
