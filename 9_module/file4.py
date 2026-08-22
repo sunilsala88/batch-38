fd2d9ed52523cbe7118404712c8fee2287e96971
sunilsala88 <sunilsala88@gmail.com>
2026-08-22 18:23:18 +0530

    aug-21

--- 9_module/file4.py
+++ 9_module/file4.py
@@ -4,6 +4,9 @@ import numpy as np
 a=np.array([1,2,3,4,5])
 print(a)
 
+import selenium
+import numpy
+numpy.array([1,2,3,4,5])
 #install
 #pip install numpy
 
@@ -19,3 +22,5 @@ print(a)
 
 #list of installed packages
 #pip list,pip freeze
+
+"usr/local/bin/python3.11/pip"
