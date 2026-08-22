cf4f141eb24e5e2ae306100ed53177f70f3b74b6
sunilsala88 <sunilsala88@gmail.com>
2026-08-22 18:34:20 +0530

    aug-22-1

---
+++ 10_numpy_pandas/numpy1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+l1=[22,33,44,55,6.6]
+print(l1)
+ar1=np.array(l1,dtype=int)
+print(ar1)
+
+l1=range(10,20)
+print(list(l1))
+
+np1=np.arange(10,20)
+print(np1)
+
+np2=np.zeros(10,dtype=int)
+print(np2)
+
+np3=np.ones(10,dtype=int)
+print(np3)
