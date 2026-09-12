de0c94125ad5fbc8e5ffb32fbc3ad689c6be8fec
sunilsala88 <sunilsala88@gmail.com>
2026-09-12 18:36:44 +0530

    sep-12-1

---
+++ 11_datetime analysis/datetime1.py
@@ -0,0 +1,15 @@
+import datetime
+
+dt1=datetime.datetime(2026, 9, 14, 10, 30, 45)
+print(dt1.weekday()) 
+
+import datetime as dt
+
+dt2=dt.datetime(2026, 9, 14, 10, 30, 45)
+print(dt2.weekday())
+
+d1=dt.date(2026, 9, 14)
+print(d1)
+
+t1=dt.time(10, 30, 45)
+print(t1)
