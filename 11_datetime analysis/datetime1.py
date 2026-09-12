10da596d89efc7f8d45bc0f9593d693f4f618381
sunilsala88 <sunilsala88@gmail.com>
2026-09-12 18:45:11 +0530

    sep-12-8

--- 11_datetime analysis/datetime1.py
+++ 11_datetime analysis/datetime1.py
@@ -5,7 +5,7 @@ print(dt1.weekday())
 
 import datetime as dt
 
-dt2=dt.datetime(2026, 9, 14, 10, 30, 45)
+dt2=dt.datetime(2026, 9, 15, 10, 30, 45)
 print(dt2.weekday())
 
 d1=dt.date(2026, 9, 14)
@@ -13,3 +13,13 @@ print(d1)
 
 t1=dt.time(10, 30, 45)
 print(t1)
+
+
+#str to datetime
+s1='2026-Aug-14 10:30:45'
+f1='%Y-%b-%d %H:%M:%S'
+dt3=dt.datetime.strptime(s1,f1)
+print(dt3)
+#int
+
+#dt
