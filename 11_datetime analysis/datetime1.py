b17823ee1079b7737b12abc1cb4838b596914ff7
sunilsala88 <sunilsala88@gmail.com>
2026-09-12 18:50:56 +0530

    sep-12-55

--- 11_datetime analysis/datetime1.py
+++ 11_datetime analysis/datetime1.py
@@ -20,6 +20,16 @@ s1='2026-Aug-14 10:30:45'
 f1='%Y-%b-%d %H:%M:%S'
 dt3=dt.datetime.strptime(s1,f1)
 print(dt3)
-#int
 
-#dt
+#int
+n1=1789217928
+dt4=dt.datetime.fromtimestamp(n1)
+print(dt4)
+
+#dt to int
+n2=dt4.timestamp()
+print(n2)
+
+#dt to str
+s1=dt3.strftime('%Y-%m-%d %H:%M:%S')
+print(s1)
